eb6aa79f57c6093ed44fc67399b1dbaafac409d4
Mahmud Qosim <42577499+Mahmudqosim@users.noreply.github.com>
2020-11-24 02:35:38 +0100

    Add files via upload

---
+++ build/precache-manifest.0eebd1dbc69269767c52d335b5092501.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb41a0b9a8af5117cfa2ec43f4e1a3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf6541d75042519152f",
+    "url": "/static/css/main.b767c80e.chunk.css"
+  },
+  {
+    "revision": "e3e0cc1995810cdf071a",
+    "url": "/static/js/2.cd0130c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd0130c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf6541d75042519152f",
+    "url": "/static/js/main.378e1368.chunk.js"
+  },
+  {
+    "revision": "567f8314a4196d05619b",
+    "url": "/static/js/runtime-main.176aa5e6.js"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "be2174fa9b861410c795b4fc0c5ebfe4",
+    "url": "/static/media/bg.be2174fa.jpg"
+  }
+]);
